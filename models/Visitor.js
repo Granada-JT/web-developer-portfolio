0dc230b6698f086613635816b08ddfcf964332cd
Jomar Tabios Granada <75769590+Granada-JT@users.noreply.github.com>
2024-08-26 17:49:43 +0800

    Release v3.3.0 (#80)
    * refactor: improve spacing and remove unnecessary comments
    
    * feat: add gulp and env variables
    
    * feat: add github worflows deploy config
    
    * feat: add gh pages dependency
    
    * refactor: change token env name
    
    * feat: add configure git email and name
    
    * refactor: remove github token
    
    * refactor: add new token
    
    * refactor: change email and name
    
    * refactor: change to peace iris
    
    * refactor: add github token
    
    * refactor: change env variables to their values
    
    * refactor: remove scripts using gulp and gh pages
    
    * refactor: remove github workflow
    
    * refactor: remove github workflows
    
    * style: add head icon and improve logo (#46)
    
    * refactor: improve responsiveness of landing section
    
    * refactor: improve about section responsiveness
    
    * refactor: change font size to 2.7 rem
    
    * fix: improved column structure flex and removed translation
    
    * feat: added fade in transition and improved messenger icon
    
    * style: center picture and  add max width 700 px
    
    * refactor: improved about me section caption
    
    * fix: add display inline block and margin auto
    
    * feat: add gulp and dotenv on staging
    
    * fix: rename to gulpfile to fix build error
    
    * fix: add margins and max widths
    
    * fix: remove carousel picture fixed height and width
    
    * fix: improve project section cards responsiveness
    
    * fix: improve project section modals responsiveness
    
    * refactor: clean up code and add initial 451px media query
    
    * fix: add header padding and increase font sizes
    
    * fix: footer note and navbar toggler
    
    * refactor: improve and remove whitespaces on index css
    
    * feat: add style lint for css linting
    
    * refactor: change case from camel to kebab for css
    
    * refactor: improve index css code style and format
    
    * refactor: add and remove recommended white space in index css
    
    * feat: add and run eslint prettier

--- models/Visitor.js
+++ models/Visitor.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
-const moment = require('moment-timezone');
+const mongoose = require("mongoose");
+const moment = require("moment-timezone");
 
 const visitorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Full name is required.']
-    },
-    emailAddress: {
-        type: String,
-        required: [true, 'Email address is required.']
-    },
-    contactNo: {
-        type: String,
-        required: [true, 'Contact number is required.'],
-    },
-    message: {
-        type: String,
-        required: [true, 'Message is required.']
-    },
-    sentOn: {
-        type: Date,
-        default: new Date()
-    },
+  name: {
+    type: String,
+    required: [true, "Full name is required."],
+  },
+  emailAddress: {
+    type: String,
+    required: [true, "Email address is required."],
+  },
+  contactNo: {
+    type: String,
+    required: [true, "Contact number is required."],
+  },
+  message: {
+    type: String,
+    required: [true, "Message is required."],
+  },
+  sentOn: {
+    type: Date,
+    default: new Date(),
+  },
 });
 
-module.exports = mongoose.model('Visitor', visitorSchema);
+module.exports = mongoose.model("Visitor", visitorSchema);
--- routes/visitor.js
+++ routes/visitor.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const visitorController = require('../controllers/visitor');
+const visitorController = require("../controllers/visitor");
 
 // This route is for capturing/saving the user input from the form
-router.post("/", visitorController.saveVisitorDetails)
+router.post("/", visitorController.saveVisitorDetails);
 
 // This route is for logging in to check my portfolio dashboard
 router.post("/login", visitorController.loginKey);
