9753659d9bc86e439224cfb054d0cc10f028aba0
Jomar Tabios Granada <75769590+Granada-JT@users.noreply.github.com>
2024-09-29 22:01:11 +0800

    Release v3.3.7 (#111)
    * refactor: improve spacing and remove unnecessary comments
    
    * feat: add gulp and env variables
    
    * feat: add github worflows deploy config
    
    * feat: add gh pages dependency
    
    * refactor: change token env name
    
    * feat: add configure git email and name
    
    * refactor: remove github token
    
    * refactor: add new token
    
    * refactor: change email and name
    
    * refactor: change to peace iris
    
    * refactor: add github token
    
    * refactor: change env variables to their values
    
    * refactor: remove scripts using gulp and gh pages
    
    * refactor: remove github workflow
    
    * refactor: remove github workflows
    
    * style: add head icon and improve logo (#46)
    
    * refactor: improve responsiveness of landing section
    
    * refactor: improve about section responsiveness
    
    * refactor: change font size to 2.7 rem
    
    * fix: improved column structure flex and removed translation
    
    * feat: added fade in transition and improved messenger icon
    
    * style: center picture and  add max width 700 px
    
    * refactor: improved about me section caption
    
    * fix: add display inline block and margin auto
    
    * feat: add gulp and dotenv on staging
    
    * fix: rename to gulpfile to fix build error
    
    * fix: add margins and max widths
    
    * fix: remove carousel picture fixed height and width
    
    * fix: improve project section cards responsiveness
    
    * fix: improve project section modals responsiveness
    
    * refactor: clean up code and add initial 451px media query
    
    * fix: add header padding and increase font sizes
    
    * fix: footer note and navbar toggler
    
    * refactor: improve and remove whitespaces on index css
    
    * feat: add style lint for css linting
    
    * refactor: change case from camel to kebab for css
    
    * refactor: improve index css code style and format
    
    * refactor: add and remove recommended white space in index css
    
    * feat: add and run eslint prettier
    
    * fix: center foot note and button
    
    * fix: full-name and profession overflow to next column
    
    * fix: add landing spacer to improve left to right animation
    
    * fix: add solid bg and align texts to center
    
    * fix: add delay removing bg black
    
    * feat: add close icon and margins
    
    * fix: remove navbar and refactor close icon on active cards
    
    * refactor: remove unnecessary and unused codes
    
    * fix: add 15px margin and text align center
    
    * fix: improve footer mobile responsiveness
    
    * fix: add margin bottom 20 on footer
    
    * fix: overflow x when modal is open
    
    * feat: add contact form validation
    
    * fix: run eslint and apply its fixes
    
    * feat: add click feature to home
    
    * style: increase arrow size to 1.5 rem
    
    * fix: change calculator app production link
    
    * refactor: prioritize calculator and update assets
    
    * chore: run eslint
    
    * feat: add calculator repository link

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ gulp.task("inject-env", () => {
   return gulp
     .src("index.html")
     .pipe(replace("G-WR44F2NEMR", process.env["GA_MEASUREMENT_ID"]))
-    .pipe(replace("https://calculator-app-granada.vercel.app/", process.env["CALCULATOR_STAGING"]))
+    .pipe(
+      replace(
+        "https://minimalistic-calculator-granada.vercel.app/",
+        process.env["CALCULATOR_STAGING"],
+      ),
+    )
     .pipe(gulp.dest("."));
 });
